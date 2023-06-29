b8c7c1d2f32318aa6379c1c5aed24c6034e481b5
Kayetic <bartek.bak@protonmail.com>
2023-06-29 17:50:20 +0100

    🐛 fix(index.js): change navbar max-height to height for proper animation ✨ feat(index.js): set top-bar height to match navbar height for consistent styling

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const hamburgerMenu = document.querySelector(".menu");
 const navbar = document.querySelector(".navbar");
 
+// setting height of top-bar element to the same height as navbar to avoid using max-height so that animations work properly
+const navbarHeight = navbar.clientHeight;
+const topBar = document.querySelector(".top-bar");
+topBar.style.height = `${navbarHeight}px`;
+
 let expanded = false; // set to false initially
 hamburgerMenu.addEventListener("click", () => {
   if (expanded) {
-    navbar.style.maxHeight = "4rem"; // Adjust max-height here
+    navbar.style.height = "4rem"; // Adjust max-height here
   } else {
-    navbar.style.maxHeight = "16rem"; // And here
+    navbar.style.height = "16rem"; // And here
   }
   navbar.classList.toggle("expanded");
   expanded = !expanded;
