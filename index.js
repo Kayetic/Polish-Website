7f707eb13bac0589f2fde9fd30120e4817aa85b7
Kayetic <bartek.bak@protonmail.com>
2023-07-09 19:38:11 +0100

    added meaningful extra links and a placeholder midjourney image

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const hamburgerMenu = document.querySelector(".menu");
 const navbar = document.querySelector(".navbar");
+const logo = document.querySelector(".logo");
 
 // setting height of top-bar element to the same height as navbar to avoid using max-height so that animations work properly
 const navbarHeight = navbar.clientHeight;
@@ -9,9 +10,15 @@ topBar.style.height = `${navbarHeight}px`;
 let expanded = false; // set to false initially
 hamburgerMenu.addEventListener("click", () => {
   if (expanded) {
-    navbar.style.height = "4rem"; // Adjust max-height here
+    navbar.style.height = "4rem";
+    logo.style.fontSize = "1.5rem";
+    logo.style.marginLeft = "0";
+    logo.style.marginTop = "0";
   } else {
-    navbar.style.height = "16rem"; // And here
+    navbar.style.height = "20rem";
+    logo.style.fontSize = "2rem";
+    logo.style.marginLeft = "1rem";
+    logo.style.marginTop = "1rem";
   }
   navbar.classList.toggle("expanded");
   expanded = !expanded;
